e563b16a19150ee4e4b8eb0bdd9da8885f1b738c
Luis Angel Rocha <luis.rocha021231@gmail.com>
2022-05-14 22:31:07 -0500

    first commit

---
+++ php-getting-started/db_config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_USER', "ilwezxqomyubwh"); // Usuario
+define('DB_PASSWORD', "6086a5d3b5c1c9b731ed638a40364d17ab6368322cea240088d218b6b83317f6"); // Password
+define('DB_DATABASE', "d3lo58g702brtn"); // Nombre de la base de datos
+define('DB_SERVER', "ec2-3-229-11-55.compute-1.amazonaws.com"); // db server
+?>
